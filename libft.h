2679eb13a1f075edf914ae69d84e9bbcffad2ec1
Santana <44216456+D4rkSantana@users.noreply.github.com>
2020-02-05 20:12:47 -0300

    Add files via upload

--- libft.h
+++ libft.h
@@ -15,6 +15,7 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 void ft_putchar(char c);
 void ft_putstr(const char *str);
@@ -28,5 +29,14 @@ char *ft_strchr(const char *s, int c);
 char *ft_strnstr(const char *s1, const char *s2, size_t len);
 char *ft_strxnstr(const char *s1, const char *s2, size_t len);
 char *ft_strdup(const char *s1);
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
+void *ft_memset(void *str, int c, size_t n);
+void *ft_memcpy(void *dst, const void *src, size_t n);
+void *ft_memccpy(void *dest, const void *src, int c, size_t n);
+void *ft_memmove(void *dst, const void *src, size_t l);
 
 #endif
