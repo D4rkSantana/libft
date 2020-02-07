5d12ea3b8f409cf8b4a9417daf6f45e350c630f5
Santana <44216456+D4rkSantana@users.noreply.github.com>
2020-02-07 15:56:26 -0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -2,7 +2,11 @@ void *ft_calloc(size_t len, size_t n)
 {
   char *s;
   
+  if (!len || !n)
+    return (NULL);
   s = malloc(len * n);
+  if (!s)
+    return (NULL);
   bzero(s, len);
   return ((void *)s);
 }
